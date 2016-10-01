bbb9cf4504e7efe9d9cff4a16ba2d42f36a44c30
j <j@z.ldev.no>
2016-10-01 03:45:29 +0200

    New commit, minor fixes

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ This is a pretty useless bot, mostly used for implementing wierd stuff, and bett
 ## Usage
 * Ensure that python3, requests and lxml packages are installed (on debian: apt-get install python3 python3-lxml python3-requests)
 * Download this piece of software
-* Copy config.json.sample to config.json
-* Edit config.json
+* Copy config.json.sample to config.json and edit config.json
 * Play ball (e.g. "python3 bot.py")
 
 ## Plans
-... not so much. OOP stuff, support for adding stuff to the bot while it's running. Perhaps even send raw command to it from the shell somehow.
+... not so much. OOP stuff, support for adding stuff to the bot while it's running. Perhaps even send raw command to it from the shell somehow. CTCP stuff?
 
 ## License
 Do whatever you want with this bot.
--- bot.py
+++ bot.py
@@ -2,13 +2,16 @@
 
 import re
 import socket
-import time
 import requests
 import random
 import json
 from lxml import html
 from sys import exit
+from time import sleep
 
+#
+# Load configs
+#
 try:
     with open('config.json') as config_file:    
         config = json.load(config_file)
@@ -17,15 +20,15 @@ except FileNotFoundError:
 except:
     exit('Error parsing config.json file')
 
-    
+#
+# Helper functions
+#
 def send_pong(msg):
     print('PONG %s\r\n' % msg)
     con.send(bytes('PONG %s\r\n' % msg, 'UTF-8'))
 
-
 def send_message(msg):
     con.send(bytes('PRIVMSG %s :%s\r\n' % (config['channel'], msg), 'UTF-8'))
-
 
 def send_nick(nick):
     con.send(bytes('NICK %s\r\n' % nick, 'UTF-8'))
@@ -37,7 +40,6 @@ def join_channel():
     con.send(bytes('JOIN %s\r\n' % config['channel'], 'UTF-8'))
 
 def part_channel(msg):
-
     con.send(bytes('PART %s :%s\r\n' % (config['channel'], msg), 'UTF-8'))
 
 def get_sender(msg):
@@ -59,6 +61,31 @@ def get_message(msg):
     result = result.lstrip(':')
     return result
 
+def parse_message(msg):
+    if len(msg) >= 1:
+        msg = msg.split(' ')
+        triggers = {
+            '!test': command_test,
+            '!lmh1': command_lmh1,
+            '!quote': command_quote
+        }
+        if msg[0] in triggers:
+            triggers[msg[0]]()
+
+
+# Should return something like:
+#  - ping
+#  - query
+#  - in_channel_message
+#  - unknown
+def determine_type(msg):
+    return True
+
+
+
+#
+# Custom functions - triggered by !<word>
+#
 def command_lmh1():
     page = html.parse('http://www.diskusjon.no/index.php?showuser=175531&tab=posts')
     send_message('LMH1 sitt siste lol:')
@@ -73,17 +100,6 @@ def command_lmh1():
         title = x.xpath('.//h3/a/text()')[0]
         time = x.xpath(".//p[@class='posted_info']/text()")[0].strip()
         send_message(' * %s: %s - %s' % (time, title[7:], url))
-
-def parse_message(msg):
-    if len(msg) >= 1:
-        msg = msg.split(' ')
-        triggers = {
-            '!test': command_test,
-            '!lmh1': command_lmh1,
-            '!quote': command_quote
-        }
-        if msg[0] in triggers:
-            triggers[msg[0]]()
 
 def command_test():
     send_message('Crodot - helt greit, ingenting spessielt')
@@ -103,21 +119,30 @@ def command_quote():
     except:
         send_message('lol, noe kræsja')
 
-con = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-con.connect((config['server'], config['port']))
-time.sleep(1)
-send_nick(config['nick'])
-time.sleep(1)
-send_user(config['nick'])
-time.sleep(1)
-join_channel()
-time.sleep(1)
-
-data = ""
-
+#
+# Exception class
+#
 class IRC_link_close(Exception):
     pass
 
+
+#
+# Connect to IRC server
+#
+con = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+con.connect((config['server'], config['port']))
+# sleep(1)
+send_nick(config['nick'])
+# sleep(1)
+send_user(config['nick'])
+# sleep(1)
+join_channel()
+# sleep(1)
+
+#
+# Main loop
+#
+data = ''
 while True:
     try:
         data = data+con.recv(1024).decode('UTF-8')
@@ -135,7 +160,6 @@ while True:
                 if line[0] == 'PING':
                     send_pong(line[1])
 
-                # Noe sendes fra kanalen
                 if line[1] == 'PRIVMSG':
                     sender = get_sender(line[0])
                     message = get_message(line)
@@ -148,7 +172,7 @@ while True:
     except socket.timeout:
         exit("Socket timeout")
         
-    except IRC_link_close():
+    except IRC_link_close:
         exit("Server closed the link")
         
     except UnicodeEncodeError:
@@ -156,7 +180,6 @@ while True:
         pass
     except KeyboardInterrupt:
         part_channel('kthnxbye')
-        sleep(1)
         exit()
     except:
         print('Unidentified exception occured')
